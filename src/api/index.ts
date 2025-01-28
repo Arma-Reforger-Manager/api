28f5d989c58f6fe57488e52a120ad813191926dd
+Flabby <plusflabby@gmail.com>
2025-01-28 06:16:54 -0500

    Update for JWT

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,13 +1,29 @@
-console.log(321) 
+
 /*
     JWT
-*/
-// import * as jwt from 'jsonwebtoken';
-// import * as fs from "fs";
-// const RSA_PRIVATE_KEY = fs.readFileSync('./jwt_private.key');
 
+    # Build Docker image
+        docker build -t docker-openssl:latest .
+    # Run container
+        docker run -it --rm -v "./:/openssl-certs" docker-openssl:latest
+    # Generate private key
+        openssl genrsa -out rsa.private 2048
+    # Generate corresponding public key
+        openssl rsa -in rsa.private -out rsa.public -pubout -outform PEM
+*/
+import jwt from 'jsonwebtoken';
+import fs from "fs";
+const RSA_PRIVATE_KEY = fs.readFileSync('certs/rsa.private');
+const RSA_PUBLIC_KEY = fs.readFileSync('certs/rsa.public');
+
+
+
+/*
+    HTTP Server
+*/
 import { createServer, IncomingMessage } from "node:http"
-let server = createServer().listen(80)
+import { randomBytes } from 'node:crypto';
+let server = createServer().listen(81)
 
 const headers = {
     'Access-Control-Allow-Origin': 'http://localhost',
@@ -20,7 +36,7 @@ async function ReadBody(req: IncomingMessage) {
         let body = "";
         req.on('readable', function () {
             const read = req.read();
-            if (read)  body += read
+            if (read) body += read
         });
         req.on('end', function () {
             res(body)
@@ -36,8 +52,28 @@ server.on("request", async (req, res) => {
         res.end();
     } else {
         const body = await ReadBody(req)
-        console.debug(body)
+        // console.debug(body)
         res.writeHead(200, { 'Content-Type': 'application/json', ...headers });
-        res.end(JSON.stringify({success: true, jwt: 'string'}));
+
+        switch (req.url) {
+            case '/log-in': {
+                let token;
+                try {
+                    token = jwt.sign({ databaseIdentifier: randomBytes(12) }, RSA_PRIVATE_KEY, { algorithm: 'PS256', expiresIn: '24h' });
+                    // const verify = jwt.verify(token, RSA_PUBLIC_KEY, { ignoreExpiration: false, maxAge: '2d' });
+                    // console.log(verify)
+                } catch (err: any) {
+                    console.error({ err })
+                    return res.end(JSON.stringify({ success: false }));
+                }
+
+                return res.end(JSON.stringify({ success: true, jwt: token }));
+                break;
+            }
+
+            default:
+                return res.end(JSON.stringify({ success: false }));
+                break;
+        }
     }
 })
